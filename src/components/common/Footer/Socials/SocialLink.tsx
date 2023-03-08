2626079458672926fe6b11b70943d2dec8a84ba5
ZakariaBenali <benalizak97@gmail.com>
2023-03-08 21:04:49 +0100

    feat: add hover effect on social link footer

--- src/components/common/Footer/Socials/SocialLink.tsx
+++ src/components/common/Footer/Socials/SocialLink.tsx
@@ -3,7 +3,13 @@ import clsx from 'clsx';
 
 const SocialLink: FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ children, className, ...rest }) => {
 	return (
-		<a className={clsx('inline-block w-7 text-secondary-400 mr-4 mb-4', className)} {...rest}>
+		<a
+			className={clsx(
+				'inline-block w-7 text-secondary-400 mr-4 mb-4 hover:-translate-y-2 translate duration-300',
+				className,
+			)}
+			{...rest}
+		>
 			{children}
 		</a>
 	);
